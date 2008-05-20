4fcacbc41ee4ed2cf0c17975e1082a64501cb300
panos.laganakos <panos.laganakos@6b2e5830-ab4d-0410-b373-a7a7cc0906dc>
2008-05-20 20:41:30 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='Sleepy Django',
+	version='0.1.0',
+	description='Build a static HTML website, with Django templates',
+	author='Panos Laganakos',
+	author_email='panos.laganakos@gmail.com',
+	url='http://code.google.com/p/sleepy-django',
+	scripts=['sleepy.py'],
+	)
