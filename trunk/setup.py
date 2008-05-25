4c76005d45bd556795f2997ee0bb2e580f7d6350
panos.laganakos <panos.laganakos@6b2e5830-ab4d-0410-b373-a7a7cc0906dc>
2008-05-25 15:37:12 +0000

    recursive dir functionality started

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 
 setup(name='Sleepy Django',
-	version='0.1.0',
+	version='0.1.1',
 	description='Build a static HTML website, with Django templates',
 	author='Panos Laganakos',
 	author_email='panos.laganakos@gmail.com',
